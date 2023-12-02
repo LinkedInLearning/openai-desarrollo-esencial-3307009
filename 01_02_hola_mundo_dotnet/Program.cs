1e67eed458d681ef47bb102e1a9d54d766fa32ab
Rodrigo Diaz Concha <rodrigo@rdiazconcha.com>
2023-12-01 18:57:09 -0800

    Initial commit

---
+++ 01_02_hola_mundo_dotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using Azure.AI.OpenAI;
+
+var key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+
+var client = new OpenAIClient(key);
+
+var options = new ChatCompletionsOptions();
+options.DeploymentName = "gpt-3.5-turbo";
+options.Messages.Add(new ChatMessage(ChatRole.User, "¡Hola Mundo!"));
+
+var response = await client.GetChatCompletionsAsync(options);
+
+foreach (var c in response.Value.Choices)
+{
+    Console.WriteLine(c.Message.Content);
+}
---
+++ 03_01_dalle/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Azure.AI.OpenAI;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dalle;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private OpenAIClient client;
+    public MainWindow()
+    {
+        InitializeComponent();
+        client = new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+        GenerationProgressBar.Visibility = Visibility.Hidden;
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var options = new ImageGenerationOptions(PromptTextBox.Text);
+        GenerationProgressBar.Visibility = Visibility.Visible;
+        var response = await client.GetImageGenerationsAsync(options);
+        LoadImageFromUrl(response.Value.Data.First().Url);
+        GenerationProgressBar.Visibility = Visibility.Hidden;
+    }
+
+    private void LoadImageFromUrl(Uri imageUri)
+    {
+        try
+        {
+            BitmapImage bitmap = new();
+            bitmap.BeginInit();
+            bitmap.UriSource = imageUri;
+            bitmap.EndInit();
+
+            GeneratedImage.Source = bitmap;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error loading image: " + ex.Message);
+        }
+    }
+}
